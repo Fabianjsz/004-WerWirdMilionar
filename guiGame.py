fce2339fcfde444a1e9895e1327fcfa5755920f1
Fabibi <fa2006bian@gmail.com>
2025-06-17 13:12:18 +0200

    Update guiGame.py

--- guiGame.py
+++ guiGame.py
@@ -8,7 +8,7 @@ Datum: 12.06.2025
 from tkinter import *
 import tkinter as tk
 from tkinter import messagebox
-from databaseUtility import fetchQuestion
+from databaseUtility import fetchQuestion, get_nr_all
 import random
 
 #Variablen
@@ -27,6 +27,29 @@ playing = True
 
 #funktionen
 
+def update_random_question():
+    Nr_tupple = get_nr_all()
+    print("laenge: ", len(get_nr_all()))
+    print("Nr: ", Nr_tupple)
+    tempNr = Nr_tupple[random.randint(0, len(Nr_tupple)-1)]
+    data = fetchQuestion(tempNr)
+    data = data[0]
+    print("data: ",data)
+    
+    # Frage in Fenster einsetzen
+    question = data[1]
+    textA = data[2]
+    textB = data[3]
+    textC = data[4]
+    textD = data[5]
+    
+    
+    
+    
+    
+    #fetchQuestion(tempNr)
+
+update_random_question()
 
 def guess(answer):
     if answer == "a":
@@ -54,32 +77,33 @@ def get_questions(fragenNr):
 
 def start_quiz():
     pass
-#main loop
+    #while True:
+      #  update_random_question()
+        
 
 
-#GUI
-quizGame = Tk()
-quizGame.title("Wer Wird Milionar?")
-quizGame.geometry("700x400")
-quizGame.config(bg="#053C5E")
+def launch_game_ui(root):
+    game = tk.Toplevel(root)
+    game.title("Wer Wird Milionar")
+    game.geometry("700x400")
+    game.config(bg="#053C5E")
+    
+    #lable
+    questionLabel = tk.Label(game, text=question, font=("Arial", 16), bg="#053C5E", fg="white", wraplength=600)
+    questionLabel.pack(pady=40)
+    
+    #Buttons
+    answerA = Button(game, text=textA, font=("Arial", 14), command=lambda: guess("a"))
+    answerA.place(x=50,y=110, height=50, width=400)
 
-#label
-questionLabel = tk.Label(quizGame, text=question, font=("Arial", 16), bg="#053C5E", fg="white", wraplength=600)
-questionLabel.pack(pady=40)
+    answerB = Button(game, text=textB, font=("Arial", 14), command=lambda: guess("b"))
+    answerB.place(x=50,y=180, height=50, width=400)
 
-#Buttons
-answerA = Button(quizGame, text=textA, font=("Arial", 14), command=lambda: guess("a"))
-answerA.place(x=50,y=110, height=50, width=400)
+    answerC = Button(game, text=textC, font=("Arial", 14), command=lambda: guess("c"))
+    answerC.place(x=50,y=250, height=50, width=400)
 
-answerB = Button(quizGame, text=textB, font=("Arial", 14), command=lambda: guess("b"))
-answerB.place(x=50,y=180, height=50, width=400)
+    answerD = Button(game, text=textD, font=("Arial", 14), command=lambda: guess("d"))
+    answerD.place(x=50,y=320, height=50, width=400)
+    
+    start_quiz()
 
-answerC = Button(quizGame, text=textC, font=("Arial", 14), command=lambda: guess("c"))
-answerC.place(x=50,y=250, height=50, width=400)
-
-answerD = Button(quizGame, text=textD, font=("Arial", 14), command=lambda: guess("d"))
-answerD.place(x=50,y=320, height=50, width=400)
-
-get_questions(random.randint(1,50))
-
-#quizGame.mainloop()
